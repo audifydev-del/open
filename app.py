b2da6f8a8a1d97179412dc050c7cbfa32459dbef
audifydev-del <audify.dev@gmail.com>
2026-05-01 17:10:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,24 @@ from ytmusicapi import YTMusic
 import yt_dlp
 import os
 
-# Tell Flask to look for HTML files in the current folder (.)
 app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def index():
-    # Now it will find index.html in the parent folder
     return render_template('index.html')
 
 @app.route('/search')
 def search():
-    query = request.args.get('q')
-    if not query:
-        return jsonify([])
-    
+    query = request.args.get('q', 'trending songs') # Default search
     try:
         yt = YTMusic()
         results = yt.search(query, filter="songs")
         songs = []
-        for s in results[:10]:
+        for s in results[:15]:
             songs.append({
                 "title": s['title'],
                 "artist": s['artists'][0]['name'],
-                "thumb": s['thumbnails'][0]['url'],
+                "thumbnail": s['thumbnails'][-1]['url'], # Best quality thumb
                 "id": s['videoId']
             })
         return jsonify(songs)
@@ -35,8 +30,8 @@ def search():
 @app.route('/stream')
 def stream():
     video_id = request.args.get('id')
-    # Using worstaudio to save RAM on your 2gb device
-    ydl_opts = {'format': 'bestaudio/worstaudio', 'quiet': True}
+    # Best for 2GB RAM: lower bitrate audio to prevent crashing
+    ydl_opts = {'format': 'wa', 'quiet': True} 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(f"https://www.youtube.com/watch?v={video_id}", download=False)
         return jsonify({"url": info['url']})
